d005fe964a90b3067265abb5c552f3bfe196126c
ma <2631987631@qq.com>
2023-10-25 20:21:16 +0800

    fix:the original configuration does not take effect

--- plugins/neo-tree.lua
+++ plugins/neo-tree.lua
@@ -1,13 +1,10 @@
 return {
   "nvim-neo-tree/neo-tree.nvim",
-  opts = function()
-    return {
-      filesystem = {
-        filtered_items = {
-          hide_hidden = false,
-          hide_gitignored = false,
-        },
-      },
+  opts = function(_, opts)
+    opts.ensure_installed = require("astronvim.utils").list_insert_unique(opts.ensure_installed, {})
+    opts.filesystem.filtered_items = {
+      hide_hidden = false,
+      hide_gitignored = false,
     }
   end,
 }
